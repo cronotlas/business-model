2e6d2f7b75967c4790e9ce0ae1ebfcb9577eca64
isyan79 <isyan79@192.168.0.17>
2023-02-20 17:07:05 +0900

    뭔지 모르겠는데 생겼

---
+++ beauty/bin/frontend/kr-beauty-vue/src/monitor.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Axios from 'axios'
+
+new Vue({
+	el: '#scope-monitor',
+	data: function() {
+		return {
+			viewModel: {
+				listUser:[],
+				paramT : 'sss',
+			}
+		}
+	}
+	,mounted: function() {
+		console.log("mounted [monitor]");
+		try {
+			this.fnInit();
+      
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	,methods: {
+    fnInit: function() {
+      let vm = this;
+      console.log("asdf");
+			
+
+
+
+			Axios.post("/main", {})
+			.then(function(response) {
+				let rs = response.data;
+				console.log(rs.listInfo);
+				vm.viewModel.listUser = rs.listInfo;
+			}).catch(function(ex) {
+				console.log(ex);
+			}); 
+    }
+  }
+});
