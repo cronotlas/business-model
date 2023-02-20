f40eec408099039cf58938fa76db937fa4d829a3
isyan <isyan@DESKTOP-FBGRJH6>
2023-02-20 15:10:07 +0900

    빈 컨트롤러 생성

---
+++ beauty/src/main/java/kr/beauty/product/controller/ProductController.java
@@ -0,0 +1,32 @@
+package kr.beauty.product.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.beauty.common.dto.UserDTO;
+import kr.beauty.main.MainViewModel;
+import kr.beauty.main.service.IMainService;
+
+@Controller
+public class ProductController {
+	
+	/*
+	 * @Autowired private IMainService mainService;
+	 */
+
+	/*
+	 * @RequestMapping(value="/main",method=RequestMethod.POST) public @ResponseBody
+	 * MainViewModel listInfoActive(@RequestBody MainViewModel vm,Model model)throws
+	 * Exception{
+	 * 
+	 * vm.setListInfo(mainService.listUser()); return vm; }
+	 */
+}
